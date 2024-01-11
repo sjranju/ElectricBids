6ba491b05b3249a35d3993505ab986e0939a9214
sjranju <sjranju@gmail.com>
2024-01-11 11:48:31 +0000

    Footer responsive

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='bg-slate-900 text-white flex flex-col justify-center items-center py-10 px-32'>
-            <p className="text-center ">&copy; 2023 ElectricBids. All rights reserved.</p>
+            <p className="text-center text-sm md:text-lg">&copy; 2023 ElectricBids. All rights reserved.</p>
         </div>
     )
 }
